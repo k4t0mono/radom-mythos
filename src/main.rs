6fd3f2ab357720dfe93a2a91f60ae18c9202fac2
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-06 20:28:17 -0300

    Primeiro rascunho

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+// An attempt to make a procedural Mythos generator
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum RelationType {
+    Parent,
+    Child,
+    Sibling
+}
+
+#[derive(Debug)]
+struct Relation {
+    relation_type: RelationType,
+    destiny: u8,
+}
+
+#[derive(Debug)]
+struct Entity {
+    name: String,
+    relations: Vec<Relation>,
+}
+
 fn main() {
     println!("Random Mythos engage");
+    
+    let mut entities: Vec<Entity> = vec![];
+    for n in 0..2 {
+        entities.push(Entity{ name: format!("Entity{}", n), relations: vec![] });
+    }
+    
+    
+    entities[0].relations.push(Relation {
+        relation_type: RelationType::Parent,
+        destiny: 1
+    });
+
+    entities[1].relations.push(Relation {
+        relation_type: RelationType::Child,
+        destiny: 0
+    });
+
+    println!("{:?}", entities);
 }
