6b537790cc1d5b7d8e0984cddc11c0e6279bccf7
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-06 21:32:57 -0300

    Consigo gerar uma relação child<-parent

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,56 @@
 // An attempt to make a procedural Mythos generator
-#![allow(dead_code)]
 
-#[derive(Debug)]
-enum RelationType {
-    Parent,
-    Child,
-    Sibling
-}
+extern crate rand;
 
-#[derive(Debug)]
-struct Relation {
-    relation_type: RelationType,
-    destiny: u8,
-}
+use rand::Rng;
 
 #[derive(Debug)]
 struct Entity {
     name: String,
-    relations: Vec<Relation>,
+    parent0: i16,
 }
 
 fn main() {
     println!("Random Mythos engage");
-    
-    let mut entities: Vec<Entity> = vec![];
-    for n in 0..2 {
-        entities.push(Entity{ name: format!("Entity{}", n), relations: vec![] });
+
+    let mut entites: Vec<Entity> = vec![];
+
+    for i in 0..10 {
+        entites.push(Entity{ name: format!("ent{}", i), parent0: -1 });
     }
-    
-    
-    entities[0].relations.push(Relation {
-        relation_type: RelationType::Parent,
-        destiny: 1
-    });
 
-    entities[1].relations.push(Relation {
-        relation_type: RelationType::Child,
-        destiny: 0
-    });
+    let n_parent_child = rand::thread_rng().gen_range(2, 7);
+    println!("n_parent_child: {}", n_parent_child);
+    
+    for _i in 0..n_parent_child {
+        let mut parent: i16 = rand::thread_rng().gen_range(0, 10);
+        let mut child: i16 = rand::thread_rng().gen_range(0, 10);
+        
+        println!("tentando: {} , {}", parent, child);
 
-    println!("{:?}", entities);
+        while parent == child {
+            println!("\t{} não pode ser pai de si msm", parent);
+            parent = rand::thread_rng().gen_range(0, 10);
+        }
+
+        while entites[child as usize].parent0 > -1 {
+            println!("\t{} já tem pai", child);
+            child = rand::thread_rng().gen_range(0, 10);
+        }
+
+        println!("parent: {}, child: {}", parent, child);
+
+        entites[child as usize].parent0 = parent;
+    }
+
+    println!();
+    for e in entites.iter() {
+        let parent = e.parent0 as usize;
+        if e.parent0 > -1  {
+            println!("{} is child of {}", e.name, entites[parent].name);
+
+        } else {
+            println!("{} is child of nobody", e.name);
+        }
+    }
 }
