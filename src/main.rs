736ef340505e617c28b8ebda344fb5aad4d35a38
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-07 00:07:25 -0300

    Gerando alguma coisa com ponteiros

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ extern crate log;
 extern crate simplelog;
 extern crate rand;
 
-//use rand::Rng;
+use rand::Rng;
+use std::fmt;
 
 #[derive(Debug)]
 struct Entity {
@@ -24,11 +25,32 @@ struct Relation<'a> {
     destiny: &'a Entity,
 }
 
+impl<'a> fmt::Display for Relation<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} is {:?} of {}", self.source.name, self.type_, self.destiny.name)
+    }
+}
+
 struct Relations<'a>(&'a mut Vec<Relation<'a>>);
 
 impl<'a> Relations<'a> {
     fn add(&mut self, type_: RelationType, source: &'a Entity, destiny: &'a Entity) {
         self.0.push(Relation{ type_, source, destiny });
+    }
+
+    fn generate_child_parent(&mut self, entites: &'a mut Vec<Entity>) {
+        let s = entites.len();
+        let n = rand::thread_rng().gen_range(3, s/2+3);
+        info!("{} relations child parent", n);
+        
+        for _i in 0..n {
+            let parent = &entites[rand::thread_rng().gen_range(0, s)];
+            let child = &entites[rand::thread_rng().gen_range(0, s)];
+
+            info!("parent: {:?}, child: {:?}", parent.name, child.name);
+            self.add(RelationType::Child, child, parent);
+            self.add(RelationType::Parent, parent, child);
+        }
     }
 }
 
@@ -44,16 +66,18 @@ fn main() {
     info!("Random Mythos engage");
 
     let mut entites: Vec<Entity> = vec![];
-    for i in 0..10 {
+    for i in 0..20 {
         entites.push(Entity{ name: format!("ent{}", i) });
     }
     
     let mut rel: Vec<Relation> = vec![];
     let mut relations = Relations(&mut rel);
     
-    relations.add(RelationType::Parent, &entites[0], &entites[1]);
-    relations.add(RelationType::Child, &entites[1], &entites[0]);
+    relations.generate_child_parent(&mut entites);
+
+    //relations.add(RelationType::Parent, &entites[0], &entites[1]);
+    //relations.add(RelationType::Child, &entites[1], &entites[0]);
 
     //for i in entites.iter() { println!("{:?}", i); }
-    //for i in relations.0.iter() { println!("{:?}", i); }
+    for i in relations.0.iter() { println!("{}", i); }
 }
