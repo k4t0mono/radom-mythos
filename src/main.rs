a83f741e211c21c9fccaf3b780a82c48905485e5
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-15 22:48:01 -0300

    TABS!!!

--- src/main.rs
+++ src/main.rs
@@ -11,215 +11,215 @@ use std::fmt;
 
 #[allow(dead_code)]
 fn write_file<'a>(name: &'a str, path: &'a str) {
-    let f = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(path);
+	let f = OpenOptions::new()
+			.write(true)
+			.create(true)
+			.open(path);
 
-    let mut file = match f {
-        Err(e) => {
-            error!("Something is terrible wrong happend while oppening the file");
-            error!("{}", e);
+	let mut file = match f {
+		Err(e) => {
+			error!("Something is terrible wrong happend while oppening the file");
+			error!("{}", e);
 
-            panic!(e)
-        },
+			panic!(e)
+		},
 
-        Ok(fl) => fl,
-    };
-    
-    match file.write_all(name.as_bytes()) {
-        Err(e) => {
-            error!("Something is terrible wrong happend while writing the file");
-            error!("{}", e);
+		Ok(fl) => fl,
+	};
+	
+	match file.write_all(name.as_bytes()) {
+		Err(e) => {
+			error!("Something is terrible wrong happend while writing the file");
+			error!("{}", e);
 
-            panic!(e)
-        },
+			panic!(e)
+		},
 
-        Ok(_) => info!("File {} writed sucessfully", path),
-    }
+		Ok(_) => info!("File {} writed sucessfully", path),
+	}
 }
 
 
 #[derive(Debug, PartialEq, Eq)]
 struct Entity {
-    name: String,
+	name: String,
 }
 
 #[allow(dead_code)]
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum RelationType {
-    Base,
-    Parent,
+	Base,
+	Parent,
 }
 
 
 struct Relations {
-    entites: Vec<Entity>,
-    relations: Vec<Vec<Option<RelationType>>>,
+	entites: Vec<Entity>,
+	relations: Vec<Vec<Option<RelationType>>>,
 }
 
 impl fmt::Debug for Relations {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut s: String = "".to_string();
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let mut s: String = "".to_string();
 
-        for i in self.relations.iter() {
-            for j in i.iter() {
-                match j {
-                    &None => s += "---",
-                    &Some(ref v) => s += &format!("{:?}", v),
-                }
-                s += "\t";
-            }
-            s += "\n";
-        }
+		for i in self.relations.iter() {
+			for j in i.iter() {
+				match j {
+					&None => s += "---",
+					&Some(ref v) => s += &format!("{:?}", v),
+				}
+				s += "\t";
+			}
+			s += "\n";
+		}
 
-        write!(f, "{}", s)
-    }
+		write!(f, "{}", s)
+	}
 }
 
 impl Relations {
-    pub fn init(size: usize) -> Relations {
-        let mut entites: Vec<Entity> = vec![];
-        for i in 0..size {
-            entites.push(Entity{ name: format!("ent{:02}", i) });
-        }
+	pub fn init(size: usize) -> Relations {
+		let mut entites: Vec<Entity> = vec![];
+		for i in 0..size {
+			entites.push(Entity{ name: format!("ent{:02}", i) });
+		}
 
-        let relations: Vec<Vec<Option<RelationType>>> = vec![vec![None; size]; size];
+		let relations: Vec<Vec<Option<RelationType>>> = vec![vec![None; size]; size];
 
-        Relations{ entites, relations }
-    }
+		Relations{ entites, relations }
+	}
 
-    fn add(&mut self, source: usize, destiny: usize, rt : RelationType) {
-        self.relations[source][destiny] = Some(rt);
-    }
+	fn add(&mut self, source: usize, destiny: usize, rt : RelationType) {
+		self.relations[source][destiny] = Some(rt);
+	}
 
-    fn adjacent_out(&mut self, vertex: usize) -> Vec<usize> {
-        let mut v: Vec<usize> = vec![];
-        
-        for j in 0..self.relations[vertex].len() {
-            match &self.relations[vertex][j] {
-                &Some(_) => v.push(j),
-                &None => (),
-            }
-        }
+	fn adjacent_out(&mut self, vertex: usize) -> Vec<usize> {
+		let mut v: Vec<usize> = vec![];
+		
+		for j in 0..self.relations[vertex].len() {
+			match &self.relations[vertex][j] {
+				&Some(_) => v.push(j),
+				&None => (),
+			}
+		}
 
-        v
-    }
+		v
+	}
 
-    fn generate_base_relation(&mut self) {
-        let s = self.entites.len();
-        trace!("self.entites: {}", s);
+	fn generate_base_relation(&mut self) {
+		let s = self.entites.len();
+		trace!("self.entites: {}", s);
 
-        let n = s as usize;
-        trace!("num relations: {}", n);
+		let n = s as usize;
+		trace!("num relations: {}", n);
 
-        for _i in 0..n {
-            let mut src = rand::thread_rng().gen_range(0, s);
-            let mut dest = rand::thread_rng().gen_range(0, s);
-            trace!("src: {:?}, dest: {:?}", src, dest);
-            
-            while src == dest  {
-                trace!("I can't be my own src");
-                dest = rand::thread_rng().gen_range(0, s);
-                trace!("New dest: {}", dest);
-            }
-            
-            while let &Some(ref _rt) = &self.relations[src][dest] {
-                trace!("You already are the src");
-                src = rand::thread_rng().gen_range(0, s);
-                trace!("New src: {}", src);
-            }
+		for _i in 0..n {
+			let mut src = rand::thread_rng().gen_range(0, s);
+			let mut dest = rand::thread_rng().gen_range(0, s);
+			trace!("src: {:?}, dest: {:?}", src, dest);
+			
+			while src == dest  {
+				trace!("I can't be my own src");
+				dest = rand::thread_rng().gen_range(0, s);
+				trace!("New dest: {}", dest);
+			}
+			
+			while let &Some(ref _rt) = &self.relations[src][dest] {
+				trace!("You already are the src");
+				src = rand::thread_rng().gen_range(0, s);
+				trace!("New src: {}", src);
+			}
 
-            let mut stack: Vec<usize> = vec![];
-            let mut verif = vec![false; n];
-            stack.push(dest);
-            trace!("Verificando ciclos");
-            while let Some(top) = stack.pop() {
-                trace!("v: {}", top);
+			let mut stack: Vec<usize> = vec![];
+			let mut verif = vec![false; n];
+			stack.push(dest);
+			trace!("Verificando ciclos");
+			while let Some(top) = stack.pop() {
+				trace!("v: {}", top);
 
-                if verif[top] || top == src {
-                    trace!("A cicle identifyed");
+				if verif[top] || top == src {
+					trace!("A cicle identifyed");
 
-                    stack.clear();
-                    verif = vec![false; n];
-                    dest = rand::thread_rng().gen_range(0, s);
-                    stack.push(dest);
+					stack.clear();
+					verif = vec![false; n];
+					dest = rand::thread_rng().gen_range(0, s);
+					stack.push(dest);
 
-                    trace!("New dest: {}",dest);
+					trace!("New dest: {}",dest);
 
-                } else {
-                    verif[top] = true;
-                    let adj = self.adjacent_out(top);
-                    for i in adj.iter() { stack.push(*i); }
-                }
-            }
+				} else {
+					verif[top] = true;
+					let adj = self.adjacent_out(top);
+					for i in adj.iter() { stack.push(*i); }
+				}
+			}
 
-            info!("src: {:?}, dest: {:?}", src, dest);
-            self.add(src, dest, RelationType::Base);
-        }
-    }
+			info!("src: {:?}, dest: {:?}", src, dest);
+			self.add(src, dest, RelationType::Base);
+		}
+	}
 
-    fn generate_dot(&mut self, name: Option<String>) {
-        let mut s: String = "digraph G {\n".to_string();
+	fn generate_dot(&mut self, name: Option<String>) {
+		let mut s: String = "digraph G {\n".to_string();
 
-        for e in self.entites.iter() {
-            s += &format!("\t{}\n", e.name);
-        }
+		for e in self.entites.iter() {
+			s += &format!("\t{}\n", e.name);
+		}
 
-        s += "\n";
-        let n = self.entites.len();
-        for i in 0..n {
-            for j in 0..n {
-                match &self.relations[i][j] {
-                    &Some(RelationType::Parent) => {
-                        s += &format!(
-                            "\t {} -> {}\n",
-                            self.entites[i].name,
-                            self.entites[j].name
-                        )
-                    },
+		s += "\n";
+		let n = self.entites.len();
+		for i in 0..n {
+			for j in 0..n {
+				match &self.relations[i][j] {
+					&Some(RelationType::Parent) => {
+						s += &format!(
+							"\t {} -> {}\n",
+							self.entites[i].name,
+							self.entites[j].name
+						)
+					},
 
-                    &Some(RelationType::Base) => {
-                        s += &format!(
-                            "\t {} -> {} [color=\"grey41\"]\n",
-                            self.entites[i].name,
-                            self.entites[j].name
-                        )
-                    },
-                    _ => s += "",
-                };
-            }
-        }
+					&Some(RelationType::Base) => {
+						s += &format!(
+							"\t {} -> {} [color=\"grey41\"]\n",
+							self.entites[i].name,
+							self.entites[j].name
+						)
+					},
+					_ => s += "",
+				};
+			}
+		}
 
-        s += "}";
-        let s_slice: &str = &*s;
-        
-        let fl: String;
-        match name {
-            None => fl = "relations.dot".to_string(),
-            Some(n) => fl = n + ".dot",
-        }
-        let fl_slice: &str = &*fl;
-        
-        write_file(&s_slice, &fl_slice);
-    }
+		s += "}";
+		let s_slice: &str = &*s;
+		
+		let fl: String;
+		match name {
+			None => fl = "relations.dot".to_string(),
+			Some(n) => fl = n + ".dot",
+		}
+		let fl_slice: &str = &*fl;
+		
+		write_file(&s_slice, &fl_slice);
+	}
 }
 
 
 fn set_logger() {
-    use simplelog::*;
-    
-    TermLogger::init(LevelFilter::Trace, Config::default()).unwrap();
+	use simplelog::*;
+	
+	TermLogger::init(LevelFilter::Trace, Config::default()).unwrap();
 }
 
 
 fn main() {
-    set_logger();
+	set_logger();
 
-    info!("Random Mythos engage");
+	info!("Random Mythos engage");
 
-    let mut relations = Relations::init(10);
-    
-    relations.generate_base_relation();
-    relations.generate_dot(None);
+	let mut relations = Relations::init(10);
+	
+	relations.generate_base_relation();
+	relations.generate_dot(None);
 }
