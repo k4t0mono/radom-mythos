b5834e8cdf09ccabad9b151f5c256b2807c79f70
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-23 22:07:08 -0300

    Add unit tests - Add unit test for existing code

--- src/test.rs
+++ src/test.rs
@@ -2,18 +2,59 @@
 
 #[cfg(test)]
 mod tests {
-    
-    #[test]
-    fn dummy_test() {
-        assert_eq!(2+2, 4);
-    }
+	use RelationType;
+	use Relations;
 
-    #[test]
-    #[should_panic]
-    fn can_not_create_relations_of_invalid_size() {
-        use Relations;
+	#[test]
+	#[should_panic]
+	fn relations_init_invalid_size_should_panic() {
+		let _r = Relations::init(0);
+	}
 
-        let r = Relations::init(0);
-    }
+	#[test]
+	fn relations_init_valid_size_should_create() {
+		let r = Relations::init(10);
+
+		assert_eq!(r.entites.len(), 10);
+	}
+
+	#[test]
+	fn relations_should_not_have_base() {
+		let rs = Relations::init(10);
+
+		let mut has_base = false;
+		for i in 0..10 {
+			for j in 0..10 {
+				let r = rs.relations[i][j];
+
+				if r.is_some() && r.unwrap() == RelationType::Base {
+					has_base = true;
+				}
+			}
+		}
+
+		assert_eq!(has_base, false);
+	}
+
+	#[test]
+	fn relations_should_be_same_type() {
+		let rs = Relations::init(10);
+
+		let mut same_type = true;
+		for i in 0..10 {
+			let adj_in = rs.adjacent_in(i);
+			if adj_in.len() == 0 { continue; }
+
+			let t = rs.relations[adj_in[0]][i].unwrap();
+			for j in 1..adj_in.len() {
+				if rs.relations[j][i].unwrap() != t {
+					same_type = false;
+				}
+			}
+		}
+
+		assert_eq!(same_type, true);
+	}
+
 
 }
